50507dc9838a167c48e51f9ef1b965daf4518628
F1xYtTT <zero.zero.zero.zero2004@gmail.com>
2022-02-22 20:59:57 +0300

    novii still

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from reviews.forms import ReviewForm
 def reviews(request):
-    return render(request,'reviews.html')
+    form = ReviewForm()
+    name_1 = request.GET.get('name')
+    email_1= request.GET.get('email')
+    review_1= request.GET.get('review')
+
+    return render (request,'reviews.html',{'name_1':name_1,'email_1':email_1,'review_1':review_1,'form':form})
+
