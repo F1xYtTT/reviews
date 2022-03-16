1c09e7e73578d6c5f67aed2e7f987c0479bd367c
F1xYtTT <zero.zero.zero.zero2004@gmail.com>
2022-03-16 16:35:35 +0300

    SVOROVAL PAROL

--- reviews/models.py
+++ reviews/models.py
@@ -8,3 +8,5 @@ class Review(models.Model):
     email = models.EmailField() 
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     review = models.TextField()
+    def __str__(self):
+        return f'Review by {self.name}<{self.email}>'
--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,30 @@ from django.shortcuts import render
 from reviews.forms import ReviewForm
 from django.shortcuts import redirect
 from reviews.models import Review
+from django.views import View
+class ReviewsView(View):
+    def get(self, request):
+        form = ReviewForm()
+        reviews = Review.objects.all()
+        return render(request, 'reviews.html', {'form': form, 'reviews': reviews})
+        
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+             data = form.cleaned_data
+             name = data.get('name')
+             email= data.get('email')
+             review= data.get('review')
+             rating = data.get('rating')
+             Review.objects.create(name = name, email = email, review = review, rating = rating)
+            ##with open('data.csv','a') as file:
+            ##   file.write(f'{name}|{email}|{review}|{rating}\n')
+             return redirect('reviews')
+        form = ReviewForm()
+        reviews = Review.odjects.all()
+        return render(request, 'reviews.html', {'form': form, 'reviews': reviews})
+
+        
 
 def reviews(request):
     if request.method == 'GET':
